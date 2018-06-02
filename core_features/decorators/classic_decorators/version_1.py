9463bf3e266baba86e5d4720f605048f9fccec0a
Hovhannes Dabaghyan <hovhannes.dabaghyan@gmail.com>
2018-06-02 17:24:58 +0400

    Classic decorator V1.

---
+++ core_features/decorators/classic_decorators/version_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Author: Hovhannes Dabaghyan
+# Matter: Classic Decorator V1.
+
+
+class Asterisk(object):
+    def __call__(self, *args, **kwargs):
+        print("AT CALL: ", args, kwargs)
+        print("****************************")
+        self.func(*args, **kwargs)
+        print("****************************")
+
+    def __init__(self, func, *args, **kwargs):
+        print("AT INIT: ", func, args, kwargs)
+        self.func = func
+
+
+@Asterisk
+def test(x):
+    print("Testing: {}".format(x))
+
+
+if __name__ == '__main__':
+    print("~~~~~~~~~~~~~~~~~~~~~ Before TEST ~~~~~~~~~~~~~~~~~~~~~")
+    print()
+    test("Mira")
